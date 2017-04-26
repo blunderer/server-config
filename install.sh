7c18961e31edb3e8e62fde05e4e5d002967046ad
Tristan  Lelong <tristan@lelong.xyz>
2017-04-27 00:40:45 +0200

    script: configure backup for servers.
    This script install relevant backup scripts and crontab.
    
    Signed-off-by: Tristan  Lelong <tristan@lelong.xyz>

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+BACKUP_FOLDER="/home/backup"
+DOCKER_FOLDER="/home/data"
+DATA_FOLDERS="$DOCKER_FOLDER"
+REMOTE_SERVERS="blunderer@backup.blunderer.org:"
+CRONFILE="/tmp/cronfile"
+
+function usage() {
+	echo "usage: $0 server=[primary|secondary]"
+	exit 1
+}
+
+function install_cron() {
+	crontab -u $1 $CRONFILE
+}
+
+# Parse options
+echo "$@" | grep "help" && usage
+
+for option in "$@"; do
+	eval "$option"
+done
+
+cd "$(dirname $0)"
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Must run as root"
+	exit 1
+fi
+
+if [ -z "$server" ]; then
+	echo "Missing server parameter"
+	usage
+	exit 1
+elif [ "$server" = "primary" ]; then
+	SERVER=master
+elif [ "$server" = "secondary" ]; then
+	SERVER=slave
+else
+	echo "Unknown server parameter"
+	usage
+	exit 1
+fi
+
+mkdir -p $BACKUP_FOLDER
+chown -R blunderer.blunderer $BACKUP_FOLDER
+cp scripts/* $BACKUP_FOLDER/
+
+# Prepare root cronfile
+cp cron/update.cron $CRONFILE
+if [ "$SERVER" = "master" ]; then
+	# Prepare user file for remote backup on master server
+	REMOTE_SERVERS_LIST=$(echo $REMOTE_SERVERS | tr " " ",")
+	sed "s,@backup@,$BACKUP_FOLDER,; s,@local@,$DOCKER_FOLDER,; s,@extra@,$REMOTE_SERVERS_LIST," cron/docker-backup.cron >> $CRONFILE
+fi
+install_cron root
+
+# Prepare user cronfile
+echo -n "" > $CRONFILE
+if [ "$SERVER" = "master" ]; then
+	for REMOTE_SERVER in $REMOTE_SERVERS; do
+		for DATA_FOLDER in $DATA_FOLDERS; do
+			DEST=$REMOTE_SERVER/$DATA_FOLDER
+			sed "s,@source@,$DATA_FOLDER,; s,@dest@,$DEST," cron/remote-backup.cron >> $CRONFILE
+		done
+	done
+fi
+
+for DATA_FOLDER in $DATA_FOLDERS; do
+	sed "s,@backup@,$BACKUP_FOLDER,; s,@source@,$DATA_FOLDER,; s,@dest@,$BACKUP_FOLDER," cron/diff-backup.cron >> $CRONFILE
+done
+
+install_cron blunderer
