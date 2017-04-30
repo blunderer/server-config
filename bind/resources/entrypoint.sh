a60b8e6456b9c438fb960f3fff311f597b8e3ab1
Tristan  Lelong <tristan@lelong.xyz>
2017-05-01 01:18:12 +0200

    bind: add rDNS record generator.
    Replaces template value with reversed servers IPs.
    
    Signed-off-by: Tristan  Lelong <tristan@lelong.xyz>

--- bind/resources/entrypoint.sh
+++ bind/resources/entrypoint.sh
@@ -5,9 +5,15 @@ if [ -f "$1" ]; then
 	exec $*
 fi
 
+function reverse_ip() {
+	echo $1 | tr "." " " |awk '{print $4 "." $3 "." $2 "." $1}'
+}
+
 SERVER_IP=$1
 PRIMARY_DNS=$2
 SECONDARY_DNS=$3
+R_PRIMARY_DNS=$(reverse_ip $PRIMARY_DNS)
+R_SECONDARY_DNS=$(reverse_ip $SECONDARY_DNS)
 
 # Tune configuration
 if [ "$SERVER_IP" = "$SECONDARY_DNS" ]; then
@@ -20,8 +26,8 @@ fi
 
 sed -i "s/@primary@/$PRIMARY_DNS/" /etc/bind/*.conf /etc/bind/named.conf.local
 sed -i "s/@secondary@/$SECONDARY_DNS/" /etc/bind/*.conf /etc/bind/named.conf.local
-
-# Set permissions
+sed -i "s/@r_primary@/$R_PRIMARY_DNS/" /etc/bind/*.conf
+sed -i "s/@r_secondary@/$R_SECONDARY_DNS/" /etc/bind/*.conf
 
 # Run named
 exec /usr/bin/supervisord -n -c /etc/default/supervisord.conf
