93f1694eed7159cdbc3e74eb2169fa554f883273
Tristan  Lelong <tristan@lelong.xyz>
2020-04-22 00:09:20 +0200

    ssl: use dynamic path for $DATA location

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-# Renew if required
-getssh -w /home/data/keys/ssl blunderer.org
+set -x
 
-PRV_KEY=/home/data/keys/ssl/blunderer.org.key
-CERT=/home/data/keys/ssl/blunderer.org.crt
-FULLCHAIN=/home/data/keys/ssl/blunderer.org/fullchain.crt
-LIGHTTPD_CERT=/home/data/keys/ssl/blunderer.org/all.pem
-CA_CERT=/home/data/keys/ssl/blunderer.org/ca.pem
+DATA="/home/new-data/keys/"
+export PATH=$PATH:/home/blunderer/Projects/getssl
+
+# Renew if required
+getssl -w $DATA/ssl blunderer.org
+
+PRV_KEY=$DATA/ssl/blunderer.org/blunderer.org.key
+CERT=$DATA/ssl/blunderer.org/blunderer.org.crt
+FULLCHAIN=$DATA/ssl/blunderer.org/fullchain.crt
+LIGHTTPD_CERT=$DATA/ssl/blunderer.org/all.pem
+CA_CERT=$DATA/ssl/blunderer.org/ca.pem
 
 # Prepare lighttpd SSL cert: concatenation of key and cert
 cat $PRV_KEY $CERT > $LIGHTTPD_CERT
 
 # Deploy to local and all remotes.
-DEST="/home/data/keys backup.blunderer.org:/home/data/keys"
+DEST="$DATA backup.blunderer.org:$DATA"
 for dest in $DEST; do
+  # main keys
+  if [ "$dest" != "$DATA" ]; then
+	  rsync -aP $DATA/ssl/ $dest/ssl/
+  fi
+
   # Deploy postfix SSL
-  rsync $PRV_KEY $dest/postfix/ssl/private/blunderer.org.key
-  rsync $CERT $dest/postfix/ssl/certs/blunderer.org.crt
+  rsync -aP $PRV_KEY $dest/postfix/ssl/private/blunderer.org.key
+  rsync -aP $CERT $dest/postfix/ssl/certs/blunderer.org.crt
   
   # Deploy lighttpd SSL
-  rsync $LIGHTTPD_CERT $dest/lighttpd/server.pem
-  rsync $CA_CERT $dest/lighttpd/ca.pem
+  rsync -aP $LIGHTTPD_CERT $dest/lighttpd/server.pem
+  rsync -aP $CA_CERT $dest/lighttpd/ca.pem
 done
